4f708360f6149d7e44e1719109ab442073c68aaa
T3ny <115883165+T3ny@users.noreply.github.com>
2025-07-24 05:56:28 +0100

    Employee Directory and Leave Directory addition and Link Configuration in Html

--- escape/escape/urls.py
+++ escape/escape/urls.py
@@ -21,4 +21,6 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.dashboard),
+    path('employee_directory/', views.employee_directory, name='employee_directory'),
+    path('leave_directory/', views.leave_directory, name="leave_directory"),
 ]
--- escape/escape/views.py
+++ escape/escape/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, "dashboard.html")
+
+def employee_directory(request):
+    return render(request, "employee_directory.html")
+
+def leave_directory(request):
+    return render(request, "leave_directory.html")
